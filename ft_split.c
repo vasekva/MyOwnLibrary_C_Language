94c774be700c5efed85e8918b444e7368246ff60
vasekva <vahonin_vasek@mail.ru>
2020-11-12 02:32:44 +0300

    very funny bug with str2(убери комментарий со второй строки в мейне

--- ft_split.c
+++ ft_split.c
@@ -11,27 +11,6 @@
 /* ************************************************************************** */
 
 #include "libft.h"
-/*
-char	*ft_do_string(char  *array, char const *str, int *ind, int len)
-{
-	int		*p_i;
-
-	p_i = ind;
-	printf("Записываю слово начиная с символа |%c| под индексом %d\n ", str[p_i], p_i);
-	while (p_i < len)
-	{
-		array[p_i] = str[p_i];
-		p_i++;
-	}
-	array[p_i] = '\0';
-	printf("Заканчивая символом |%c| под индексом %d\n", str[p_i - 2], p_i -2);
-
-	printf("\n--------------------- StrLength: %d (%d)---------------------------\n", len, len - 1);
-	printf("\n---------------------- String: %s------------------------------\n", array);
-
-	return (array);
-}
-*/
 
 int		ft_countWords(char const *str, char c)
 {
@@ -52,53 +31,8 @@ int		ft_countWords(char const *str, char c)
 	}
 	printf("\n-------------------- Numbers of String: %d -------------------\n", words);
 	return (words);	
-}	
-/*
-int		ft_len_first_str(char const *str, char c)
-{
-	char *s = {"Hello World I'm Back and i always will be there"};
-
-	int 		len;
-	int 		end;
-	int 		start;
-
-	len = 0;
-	end = 0;
-//	while (!((*s == c && *(s + 1) != c && *(s + 1) != '\0') || (i == 0 && *s != c)))
-//	{
-//		if (*s == c || *s == '\0')
-//		{
-//			end = len - 1;
-//			if (*s == '\0')
-//				end++;
-//			len = end + 1;
-//			return (len);
-//		}
-//		len++;
-//	}
-	int i = 0;
-	while (*s != '\0')
-	{
-		if (*s !=c && *s != '\0' && (*(s - 1) == c || *(s - 1) == '\0' || i == 0))
-		{
-			printf("\nСлово началось под индексом %d\n", i);
-			start = i;
-		}
-		if (*(s + 1) == c || *(s + 1) == '\0')
-		{
-			len = i - start + 1;
-			//end = i;
-			printf("\nСлово длинной %d закончилось символом %c под индексом %d\n", len, *s, i);
-			len = 0;
-			return (start, len);
-		}
-		//len++;
-		i++;
-		s++;
-	}
-	return (ft_strlen(s));
 }
-*/
+
 char	**ft_split(char const *s, char c)
 {
 	char	**matrix_for_string;
@@ -116,61 +50,46 @@ char	**ft_split(char const *s, char c)
 	len = 0;
 	array_index = 0;
 	num_words = ft_countWords(s, c); // считает кол-во слов в целом в строке
-	matrix_for_string = (char **)malloc(num_words * sizeof(char *)); // выделяем память под все строки
+	matrix_for_string = (char **)malloc(num_words * sizeof(char *) + 1); // выделяем память под все строки
 	if (matrix_for_string == NULL)
 		return (NULL);
 
 	printf("\n =================== В твоей строке %d слов(а) ========================\n", num_words);
 
-	while (*s != '\0')
+	while (s[i] != '\0')
 	{
-		if (i == 0 || ((*s != c && *s != '\0') && (*(s - 1) == c || *(s - 1) == '\0')))
+		while (s[i] == ' ' && s[i] != '\0')
+		{
+			i++;
+		}
+		if (i == 0 || ((s[i] != c && s[i] != '\0') && (s[i - 1] == c || s[i - 1] == '\0')))
 		{
 			printf("\n ----------------------------------------------------------------\n");
-			printf("\nСлово началось под индексом %d символом |-%c-| \n", i, s[0]);
+			printf("\nСлово началось под индексом %d символом |-%c-| \n", i, s[i]);
 			start = i;
 		}
-		if (*(s + 1) == c || *(s + 1) == '\0')
+		if (s[i + 1] == c || s[i + 1] == '\0')
 		{
 			len = i - start + 1;
 			//end = i;
-			printf("\nСлово длинной %d закончилось символом |-%c-| под индексом %d\n", len, *s, i);
+			printf("\nСлово длинной %d закончилось символом |-%c-| под индексом %d\n", len, s[i], i);
 			printf("\nПередаю в substr строку с началом под индексом %d и длинной %d \n", start, len);
 			printf("\nЗаписываю в матрицу итоговую строку в ячейку под номером %d\n", array_index);
 			printf("\n ----------------------------------------------------------------\n");
-			matrix_for_string[array_index] = ft_substr(s, 0, len);
+			matrix_for_string[array_index] = ft_substr(s, i - len + 1, len);
 			printf("\nРезультат: %s", matrix_for_string[array_index]);
 			array_index++;
 			len = 0;
 		}
-//		if (((*s == c && *(s + 1) != c && *(s + 1) != '\0') || (i == 0 && *s != c)) && (num_words > kek))
-//		{
-//			len = ft_len_first_str(s, c);
-//			array = malloc(sizeof(*s) * (len + 1));
-//			array = ft_substr(s, i, len);
-//			printf("\n----------------Создаю строку начиная с индекса %d длинной %d символов--------------\n\n", i, len + 1);
-//			matrix_for_string[i] = array;
-//			//printf("\n----------------Передвигаем указатель на %d ячеек--------------\n\n", len + 1);
-//			//s += len + 1;
-//			kek++;
-//		}
 		i++;
-		s++;
 	}
-	//i = 0;
-//	printf("\n\n\n\n\n");
-//	for (i = 0; i < num_words; i++) {
-//		//for (i = 0; i < ft_strlen(matrix_for_string[i]; i++)) {
-//			printf("ind %d: %s-\n", i, matrix_for_string[i]);
-//		//}
-//	}
 	return (matrix_for_string);
 }
 
 int		main(void)
 {
-	char *str1 = {"Hello World I'm Back and i always will be there"};
-	char *str2 = {"Hello World and i don't know what to write!!!"};
+	char *str1 = {"     Hello       World      I'm    Back    and   i always will be there            "};
+	//char *str2 = {"Hello World and i don't know what to write!!!"};
 	//char *str2 = {"123 456 789 0"};
 
 	//char *str1;
