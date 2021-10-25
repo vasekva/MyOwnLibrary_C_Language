16c8f1b08e91a591098682705f0b1ade7657050b
vasekva <vahonin_vasek@mail.ru>
2021-10-26 01:06:37 +0300

    ft_atoi.c && ft_itoa.c have been changed

--- srcs/first_part/ft_atoi.c
+++ srcs/first_part/ft_atoi.c
@@ -12,54 +12,23 @@
 
 #include "libft.h"
 
-static size_t	iteration(const char *str, int index, int neg)
+int	ft_atoi(const char *str)
 {
-	size_t i;
-	size_t count;
-	size_t res;
+	int		i;
+	int		num;
+	int		sign;
 
-	res = 0;
-	count = 0;
-	i = index;
-	while (str[i] > 47 && str[i] < 58)
-	{
-		if (count > 20)
-		{
-			if (neg < 0)
-				return (0);
-			else
-				return (-1);
-		}
-		res = res * 10 + str[i++] - '0';
-		count++;
-	}
-	return (res);
-}
-
-int				ft_atoi(const char *str)
-{
-	int	i;
-	int neg;
-	int res;
-	int count;
-	int flag;
-
-	flag = 0;
 	i = 0;
-	neg = 1;
-	res = 0;
-	count = 0;
-	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
+	num = 0;
+	sign = 1;
+	while (str[i] == '\n' || str[i] == '\t' || str[i] == '\r'
+		|| str[i] == '\v' || str[i] == '\f' || str[i] == ' ')
 		i++;
-	while (str[i] == 45 || str[i] == 43)
-	{
-		if (flag > 0)
-			return (0);
-		flag++;
-		if (str[i] == 45)
-			neg = -neg;
+	if (str[i] == '-')
+		sign = -1;
+	if (str[i] == '-' || str[i] == '+')
 		i++;
-	}
-	res = iteration(str, i, neg);
-	return (res * neg);
+	while (str[i] && str[i] >= '0' && str[i] <= '9')
+		num = num * 10 + (str[i++] - '0');
+	return (num * sign);
 }
